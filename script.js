e2170d694a3f991048a4ebe489f565c7acd0e41e
Mohanad <mohanad5ayman@gmail.com>
2025-11-30 19:23:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn');
         removeBtn.onclick = function () {
             taskList.removeChild(li);
         };
